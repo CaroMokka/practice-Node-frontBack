c0840883c05dd018f28a98f95eab412399f7ab8f
carolina <ci.arayagonzalez@gmail.com>
2024-10-21 20:39:20 -0300

    add-avances registro de personas

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express()
 
 const port = 3000
 
-app.listen(() => {
+app.use("/public/", express.static(`${__dirname}/assets`))
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/views/index.html`)
+})
+
+app.listen( port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`)
 })
