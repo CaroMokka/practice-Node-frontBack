72a81e3a9a1f731e5bfb611052a82d70cafab755
carolina <ci.arayagonzalez@gmail.com>
2024-10-22 23:27:52 -0300

    add-validaciones desde api + front

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,13 +11,12 @@ $(() => {
     const nombre = $("#txt-nombre").val();
     const apellido = $("#txt-apellido").val();
 
-    if(rut == "") return $("#msn-rut").html("Ingrese un rut válido");
-    if(nombre == "") return $("#msn-nombre").html("Ingrese un nombre");
-    if(apellido == "") return $("#msn-apellido").html("Ingrese un apellido");
+    // if(rut == "") return $("#msn-rut").html("Ingrese un rut válido");
+    // if(nombre == "") return $("#msn-nombre").html("Ingrese un nombre");
+    // if(apellido == "") return $("#msn-apellido").html("Ingrese un apellido");
 
-    // rut != "" ? rut : $("#msn-rut").html("Ingrese un rut válido");
-    // nombre != "" ? nombre : $("#msn-nombre").html("Ingrese un nombre");
-    // apellido != "" ? apellido : $("#msn-apellido").html("Ingrese un apellido");
+    $(".error").addClass("d-none")
+
 
     $.ajax({
       method: "GET",
@@ -31,21 +30,42 @@ $(() => {
         // alert("ok")
       },
       error: function (error) {
-        alert("Error");
+          if(error.status == 409){
+              $("#alert-validation").removeClass("d-none").html(error?.responseJSON?.message || "")
+          } else {
+              $("#alert-error").removeClass("d-none").html(error?.responseJSON?.message || "Error interno")
+          }
+      
       },
     });
 
+    function listarRegistro(listado){
+        $("#table tbody").html("")
+        listado.forEach(persona => {
+           
+            $("#table tbody").append(`
+                <tr>
+                <td>${persona.id}</td>
+                <td>${persona.rut}</td>
+                <td>${persona.nombre}</td>   
+                <td>${persona.apellido}</td>
+                </tr>
+            `);
+        });
+    }
+
     $.get(`${URL_BASE}/listar-registro`)
       .done(function (response) {
-        console.log("Éxito de respuesta", response);
+        const { data } = response
+        listarRegistro(data)
+          
       })
-      .fail(function (jqXHR, errorThrown) {
-        console.log(
-          "Error de respuesta",
-          jqXHR.status,
-          jqXHR.statusText,
-          errorThrown
-        );
+      .fail(function (error) {
+        $("#table tbody").html(`
+                <tr>
+                    <td colspan="4" class="text-center text-danger">No es posible consultar las personas registradas</td>
+                </tr>
+            `);
       });
   });
 });
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const { v4: uuidv4 } = require('uuid');
 const fs = require("fs")
-const clc = require("cli-color")
+const clc = require("cli-color");
+const { response } = require("express");
+const { validate, clean, format, getCheckDigit } = require('rut.js')
 
 const port = 3000
 const app = express()
@@ -15,27 +17,54 @@ app.get("/", (req, res) => {
 })
 
 app.get("/registrar-persona", (req, res) => {
-    const rut = req.query.rut
+    let rut = req.query.rut
     const nombre = req.query.nombre
     const apellido = req.query.apellido
 
-    const persona = {
-        id: uuidv4(),
-        rut,
-        nombre,
-        apellido
+    try {
+        if(rut == "") { return res.status(409).json({ "message": "Ingresar rut" }) }
+        if(!validate(rut)) {
+            return res.status(409).json({ "message": "Rut inválido" })
+        }
+        rut = format(rut)
+
+        
+     
+
+        //Línea para forzar error
+        //throw "Error de registro"
+
+        const persona = {
+            id: uuidv4(),
+            rut,
+            nombre,
+            apellido
+        }
+        console.log(persona)
+    
+        const contentTxt = fs.readFileSync(dataFile, "utf-8")
+        const contentJS = JSON.parse(contentTxt)
+    
+        const busqueda = contentJS.find( item => item.rut === rut )
+        if(busqueda) return res.status(409).json({ "message": "RUT registrado previamente"})
+
+        if(nombre == "") { return res.status(409).json({ "message": "Ingresar nombre" }) }
+        if(apellido == "") { return res.status(409).json({ "message": "Ingresar apellido" }) }
+    
+        contentJS.push(persona)
+    
+        fs.writeFileSync(`${dataFile}`, JSON.stringify(contentJS), "utf-8")
+    
+
+        res.json({ message: "Registro éxitoso", data: persona })
+    } catch(err){
+        response.status(500).json({"message": "Ocurrió un error, intente más tarde."})
     }
+    
 
-    const contentTxt = fs.readFileSync(dataFile, "utf-8")
-    const contentJS = JSON.parse(contentTxt)
-
-    const busqueda = contentJS.find( item => item.rut === rut )
-    if(busqueda) return console.log(clc.yellow("Esta persona ya se encuentra registrada."))
-
-    contentJS.push(persona)
-    fs.writeFileSync(`${dataFile}`, JSON.stringify(contentJS), "utf-8")
-    console.log(contentJS)
-    res.json({ message: "Registro éxitoso", data: persona })
+   
+  
+    
 })
 
 app.get("/listar-registro", (request, response) => {
